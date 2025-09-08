57c14e60b87da9c98364029b09b6e0631271a454
Rendszergazda <rendszergazda@blathy.info>
2025-09-08 15:14:28 +0200

    Add project files.

---
+++ kaveautomata/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace kaveautomata
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ kaveautomata/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace kaveautomata
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        int ar_vanilia = 400;
+        int ar_fekete = 250;
+        int ar_cappuchino = 300;
+        int ar_latta = 300;
+        int ar_tejk = 400;
+        int ar_jeges = 450;
+        int ar_forrocs = 500;
+        int ar_forrov = 50;
+        int ar_cuk = 20;
+        int menny_cuk = 0;
+        int kávé = 0;
+
+        private void vaniliaskave_Click(object sender, RoutedEventArgs e)
+        {
+            kávé = 400;              
+        }
+
+        private void Pluszcukor_Click(object sender, RoutedEventArgs e)
+        {
+            menny_cuk = menny_cuk + 1;
+        }
+
+        private void Minuszcukor_Click(object sender, RoutedEventArgs e)
+        {
+            if (menny_cuk != 0) 
+            {
+                menny_cuk = menny_cuk - 1;
+            }
+        }
+
+        private void rendelés_Click(object sender, RoutedEventArgs e)
+        {
+            int osszeg = kávé + (menny_cuk*ar_cuk);
+            MessageBox.Show((osszeg).ToString() + "ft");
+            kávé = 0;
+            menny_cuk = 0;
+        }
+    }
+}
